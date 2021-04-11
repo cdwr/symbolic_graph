18c31828411a08b903ea34a5a9a8e74d9f402883
cwilson1 <wilsoncdwr@gmail.com>
2021-04-11 11:53:23 -0700

    test added, random things in orginal

--- 350BBD.py
+++ 350BBD.py
@@ -11,6 +11,9 @@ def toBinary():
         i+=1
     return L
 
+def is_prime(n):
+    return n > 1 and all(n % i for i in islice(count(2), int(sqrt(n)-1)))
+
 def findEdge():
     edges = {}
     i = 0
@@ -33,3 +36,31 @@ def appendEdges():
         eb.append(L[i[0]] + L[i[1]])
     return eb
 
+class edge:
+    def __init__(self, i, j):
+        seld.prime = 
+        pass
+    def 
+    def 
+
+class node:
+    def __init__(self, n):
+        self.prime = is_prime(n)
+        self.even = (n%2) == 0
+        self.bin = "{0:b}".format(n)
+        self.edges = self.getEdges()
+        pass
+    def degEdges(self):
+
+    def 
+
+
+class BDD:
+    def __init__():
+        pass
+
+    def __str__():
+        pass
+    def 
+
+# 5 -> 00101 -> ~x1 & ~x2 & x3 & ~x4 & x5
